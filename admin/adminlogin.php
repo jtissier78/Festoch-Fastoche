bb0b5862509fdf83d246ce3da85df4b2890c3afa
Coura <afor.rui@gmail.com>
2019-11-26 11:33:32 +0100

    origin

--- admin/adminlogin.php
+++ admin/adminlogin.php
@@ -1,7 +1,12 @@
 <?php 
 
 //start function by default set to visitor
-session_start();
+
+
+if (session_status() == PHP_SESSION_NONE) { //verify if session is already started
+    session_start();
+}
+$_SESSION['userName'] = "visitor";
 if(isset($_SESSION['userName'])) {
   echo "Your session is running " . $_SESSION['userName'];
 }
@@ -13,12 +18,23 @@ else {
 
 
 <body>
+
     <div> <!-- your main div/section -->
-        <form action="verifylog.php" method="post" class="form_user">  <!-- attention to url path -->
-            <h6>Username:</h6> <input type="text" name="adminlog"><br>
-            <h6>PassWord:</h6> <input type="text" name="pwlog"><br><br>
-            <input type="submit">
-        </form>
+
+    <?php
+        $user = $_SESSION['userName'];
+
+        if(strcmp($user,"visitor")==0){ //verify if user is connected, if not charges form.
+            echo "<form action='verifylog.php' method='post' class='form_user'>";  //attention to url path
+            echo "<h6>Username:</h6> <input type='text' name='adminlog'><br>";
+            echo "<h6>PassWord:</h6> <input type='text' name='pwlog'><br><br>";
+            echo "<input type='submit'>";
+            echo "</form>";
+        }
+        else {
+            echo "<h3>You are already connected!!</h3>";
+        }
+    ?>
     </div>
 
 </body>
